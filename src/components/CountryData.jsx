3c423e397e62d3cdf03297b0fad0aa7a90779cb6
Maciej Lazarewicz <maciej.lazarewicz@gmail.com>
2024-01-19 00:01:00 +0100

    feat: Country Features

--- src/components/CountryData.jsx
+++ src/components/CountryData.jsx
@@ -8,36 +8,40 @@ function CountryData() {
   const { name } = useParams();
   const [countryData, setCountryData] = useState(null);
   const [governmentData, setGovernmentData] = useState(null);
+  const [flags, setFlags] = useState(null);
 
   useEffect(() => {
-    const fetchCountryData = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/data/${name}`);
-        const responseData = await response.json();
-        setCountryData(responseData);
-      } catch (error) {
-        console.error('error:', error);
-      }
-    };
-    const fetchGovernmentData = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:5000/government-type/${name}`
-        );
-        const governmentData = await response.json();
+        const [countryResponse, flagResponse, governmentResponse] =
+          await Promise.all([
+            fetch(`http://localhost:5000/data/${name}`),
+            fetch(`http://localhost:5000/flags/${name}`),
+            fetch(`http://localhost:5000/government-type/${name}`),
+          ]);
+
+        const countryData = await countryResponse.json();
+        setCountryData(countryData);
+
+        const flagData = await flagResponse.json();
+
+        setFlags(flagData);
+
+        const governmentData = await governmentResponse.json();
         setGovernmentData(governmentData[0]);
       } catch (error) {
         console.error('error:', error);
       }
     };
-    fetchGovernmentData();
 
-    fetchCountryData();
+    fetchData();
   }, [name]);
+
+  const allData = countryData && governmentData && flags;
 
   return (
     <Box>
-      {countryData && governmentData && (
+      {allData && (
         <Box key={countryData.country}>
           <Country
             name={name}
@@ -49,6 +53,7 @@ function CountryData() {
             cities={countryData.cities}
             coordinates={countryData.coordinates}
             government={governmentData.government}
+            flag={flags.flagBase64}
           />
         </Box>
       )}
